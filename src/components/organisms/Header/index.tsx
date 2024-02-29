60e4b06c159444b2255537534d5fa531ee150201
Mirshod01 <mirshod23.roziqov@gmail.com>
2024-02-29 11:28:29 +0500

    update: header component

--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -1,61 +1,43 @@
 import "./index.scss";
 import { useAppDispatch, useAppSelector } from "../../../share/redux/hook";
-import { useEffect } from "react";
-import { Link } from "react-router-dom";
-import { setCurrentLanguage } from "share/redux/common";
+import { Link, useNavigate } from "react-router-dom";
 import { Image } from "components/atoms";
-
 import IconsFile from "assets/icons";
 import { Cart } from "components/molecules";
+import { connectWallet } from "share/redux/metamask/thunks";
 
 
 const Header: React.FC = () => {
   const dispatch = useAppDispatch();
-  const currentLanguage = useAppSelector(
-    (state) => state.common.currentLanguage,
-  );
+  const navigate = useNavigate()
 
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const header = await import(`./data/${currentLanguage}.ts`);
-      console.log(header)
-    };
-    fetchData();
-  }, [currentLanguage]);
-  // eslint-disable-next-line
-  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const { value } = e.target;
-    dispatch(setCurrentLanguage(value));
-  };
-  // eslint-disable-next-line
-  const goBack = () => {
-    console.log("da")
+  const account = useAppSelector((state) => state.metamask.account);
+  function isMobileDevice() {
+    return 'ontouchstart' in window || 'onmsgesturechange' in window;
   }
+  const connectMetamask = async () => {
+    try {
+      if (!account) {
+        if (isMobileDevice()) {
+          const dappUrl = process.env.REACT_APP_PUBLIC_API_URL || ''; // TODO enter your dapp URL. For example: https://uniswap.exchange. (don't enter the "https://")
+          window.location.href = "https://metamask.app.link/dapp/" + dappUrl.split('//')[1] + '/trade'
+        }
+        dispatch(connectWallet())
+      }
+      navigate("/my-points")
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
     <div id="navbar">
       <Link to="/" >
         <Image src={IconsFile.Logo} />
       </Link>
-      {/* {pageData.map((item, index) => (
-        <Link to={item.link} key={item.link + index}>
-          {" "}
-          {item.title}
-        </Link>
-      ))}
-      <select
-        className="select_language"
-        onChange={handleChange}
-        value={currentLanguage}
-      >
-        <option defaultValue="en">en</option>
-        <option value="kr">kr</option>
-        <option value="ru">ru</option>
-      </select> */}
       <div className="left_side">
-        <Link to="/about" >
+        <div onClick={connectMetamask} className="login" >
           <Image src={IconsFile.Profile} />
-        </Link>
+        </div>
         <Link to="/cart" >
           <Cart />
         </Link>
