26d16d4aa87e7c81f04b2c843354956a81e981aa
meritz3695 <meritz3695@gmail.com>
2024-03-27 23:48:58 +0900

    fix: mobile connect

--- src/customHooks/useConnect.ts
+++ src/customHooks/useConnect.ts
@@ -2,7 +2,6 @@ import { useAppDispatch, useAppSelector } from "share/redux/hook";
 import { logout } from "share/redux/metamask";
 import { connectWallet } from "share/redux/metamask/thunks";
 
-
 const useConnect = () => {
   const dispatch = useAppDispatch();
 
@@ -14,16 +13,15 @@ const useConnect = () => {
   //   }
   // }, [account]);
   function isMobileDevice() {
-    return 'ontouchstart' in window || 'onmsgesturechange' in window;
+    return "ontouchstart" in window || "onmsgesturechange" in window;
   }
   const connectMetamask = async () => {
     try {
       if (!account) {
         if (isMobileDevice()) {
-          const dappUrl = process.env.REACT_APP_PUBLIC_API_URL || ''; // TODO enter your dapp URL. For example: https://uniswap.exchange. (don't enter the "https://")
-          window.location.href = "https://metamask.app.link/dapp/" + dappUrl.split('//')[1] + '/trade'
+          window.open("https://wallet.roninchain.com", "_blank");
         }
-        dispatch(connectWallet())
+        dispatch(connectWallet());
       }
     } catch (e) {
       console.log(e);
@@ -32,7 +30,7 @@ const useConnect = () => {
   const handleLogoutAndConnect = async () => {
     // Dispatch logout action
     await dispatch(logout());
-    sessionStorage.removeItem('token');
+    sessionStorage.removeItem("token");
     window.location.reload();
     // Call connectMetamask after logout
     connectMetamask();
@@ -40,7 +38,7 @@ const useConnect = () => {
 
   return {
     connectMetamask,
-    handleLogoutAndConnect
+    handleLogoutAndConnect,
   };
 };
 export default useConnect;
