08cb5b757d161e5b7e065455338245000ce8f2b1
meritz3695 <meritz3695@gmail.com>
2024-03-28 03:04:18 +0900

    fixxxxx

--- src/customHooks/useConnect.ts
+++ src/customHooks/useConnect.ts
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from "react";
-import { useAppDispatch } from "share/redux/hook";
+import { useAppDispatch, useAppSelector } from "share/redux/hook";
 import { logout } from "share/redux/metamask";
 import { connectWallet } from "share/redux/metamask/thunks";
 import { WCEvent, WalletSDK } from "@roninnetwork/wallet-sdk";
@@ -10,7 +10,7 @@ function isMobileDevice() {
 
 const useConnect = () => {
   const dispatch = useAppDispatch();
-  // const account = useAppSelector((state) => state.metamask.account);
+  const account = useAppSelector((state) => state.metamask.account);
   const sdkRef = useRef<any>(null);
 
   useEffect(() => {
@@ -23,7 +23,7 @@ const useConnect = () => {
   }, []);
 
   async function connectRonin() {
-    await sdkRef.current.connect();
+    if (sdkRef.current) await sdkRef.current.connectMobile();
     sdkRef.current.on(WCEvent.DISPLAY_URI, (wcUri: string) => {
       window.open(
         `https://wallet.roninchain.com/auth-connect?uri=${wcUri}`,
@@ -34,11 +34,13 @@ const useConnect = () => {
 
   const connectMetamask = async () => {
     try {
-      if (isMobileDevice()) {
-        await connectRonin();
-        return;
-      } else {
-        dispatch(connectWallet());
+      if (!account) {
+        if (isMobileDevice()) {
+          await connectRonin();
+          return;
+        } else {
+          dispatch(connectWallet());
+        }
       }
     } catch (e) {
       console.log(e);
